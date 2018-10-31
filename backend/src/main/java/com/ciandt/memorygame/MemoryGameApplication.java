0da6998778152ca4f012fb618d5f2fa546147dad
Marco Aurélio Batista Corrêa <marcoabcorrea@gmail.com>
2018-10-30 23:07:01 -0300

    Initial commit with scaffolding

---
+++ backend/src/main/java/com/ciandt/memorygame/MemoryGameApplication.java
@@ -0,0 +1,12 @@
+package com.ciandt.memorygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryGameApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/ciandt/memorygame/MemoryGameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ciandt.memorygame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemoryGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
